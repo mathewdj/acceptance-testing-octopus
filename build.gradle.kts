f4ab826fa5968f7c8410459515c1eb08c6a315f4
mathewdj <9652156+green-seagull@users.noreply.github.com>
2023-05-31 06:34:17 +1000

    Add spring boot

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,16 @@
 plugins {
     kotlin("jvm") version "1.8.21"
+
+    kotlin("plugin.spring") version "1.6.21"
+    id("org.springframework.boot") version "2.7.12"
+    id("io.spring.dependency-management") version "1.0.15.RELEASE"
+
     id("io.gitlab.arturbosch.detekt") version "1.22.0"
 }
 
 group = "local.mathewdj.acceptance.testing.octopus"
 version = "0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
 
 allprojects {
     repositories {
@@ -14,12 +20,24 @@ allprojects {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
     testImplementation("org.assertj:assertj-core:3.24.2")
     testImplementation(platform("org.junit:junit-bom:5.9.2"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
 
     testImplementation("io.cucumber:cucumber-java:7.12.0")
     testImplementation("io.cucumber:cucumber-junit:7.12.0")
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "11"
+    }
 }
 
 tasks.test {
