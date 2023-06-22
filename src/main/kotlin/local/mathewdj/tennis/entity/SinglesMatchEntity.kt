b1beee49fe85f580bda4f603e947d499e1ce2793
mat.johnson <98291059+matdjohnson@users.noreply.github.com>
2023-06-23 07:17:06 +1000

    Change entity id type

--- src/main/kotlin/local/mathewdj/tennis/entity/SinglesMatchEntity.kt
+++ src/main/kotlin/local/mathewdj/tennis/entity/SinglesMatchEntity.kt
@@ -14,7 +14,7 @@ import java.time.LocalDateTime
 data class SinglesMatchEntity(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: String? = null,
+    val id: Long? = null,
 
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
